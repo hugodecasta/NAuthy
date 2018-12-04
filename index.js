eef8e0fd47b19be0192a945a0fc39d7186ce406e
p1608557 <hugo.castaneda@etu.univ-lyon1.fr>
2018-12-04 10:16:47 +0100

    set base module interfaces and submodules

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 'use strict'
+
+const retriever = require('./retrievalMethods.js')
+
+// --------------------------------------------------------------
+// --------------------------------------------- INTERNAL -------
+var tokenMap = {}
+
+function createToken(userKey) {
+
+}
+
+// --------------------------------------------------------------
+// -------------------------------------------- INTERFACE -------
+// ------------------------------------------------------ CONNECT
+exports.requireToken = function(userKey) {
+
+}
+exports.connect = function(userKey, token) {
+
+}
+exports.disconnect = function(userKey, token) {
+
+}
+// ------------------------------------------------------ INFO RETRIEVAL
+exports.userHasRight = function(userKey, token, right) {
+
+}
+// ------------------------------------------------------ 
+exports.createUser = function(userKey, token, newUserKey, baseRole) {
+
+}
+exports.addUserRole = function(userKey, token, newUserKey, role) {
+
+}
+exports.addRole = function(userKey, token, role) {
+
+}
+exports.addRoleRight = function(userKey, token, role, right) {
+
+}
---
+++ retrievalMethods.js
@@ -0,0 +1,45 @@
+'use strict'
+
+// ------- fake MONGO
+// ------- fake MONGO
+
+// ---------------------------------------- USERS RETRIEVAL
+exports.retrieveAllUsers = function() {
+
+}
+exports.userExists = function(userKey) {
+
+}
+exports.retrieveOneUser = function(userKey) {
+
+}
+// ---------------------------------------- USER MANAGE
+exports.createUser = function(userKey, baseRole) {
+
+}
+exports.removeUser = function(userKey) {
+
+}
+exports.addUserRole = function(userKey, role) {
+
+}
+exports.removeUserRole = function(userKey, role) {
+
+}
+// ---------------------------------------- ROLE
+exports.roleExists = function(roleName) {
+
+}
+exports.createRole = function(roleName, baseRights) {
+
+}
+exports.removeRole = function(roleName) {
+
+}
+exports.addRoleRight = function(roleName, right) {
+
+}
+exports.removeRoleRight = function(roleName, right) {
+
+}
+// ----------------------------------------
