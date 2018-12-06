e5f3456bfb8baf0bfd07285b4acd9657198fb789
p1608557 <hugo.castaneda@etu.univ-lyon1.fr>
2018-12-06 09:49:14 +0100

    Update files with MIT License detail

--- module_tester.js
+++ module_tester.js
@@ -1,3 +1,9 @@
+//-----------------------------------------------------------------------------
+// Copyright (C) 2018 Hugo Castaneda
+// Licensed under the MIT license.
+// See LICENSE.md file in the project root for full license information.
+//-----------------------------------------------------------------------------
+
 'use strict'
 
 const NAuthy = require('./')
